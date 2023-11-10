9d0cf18e90400937280eb93ed56b4a716d2d873f
Jesper Stroem js@ece.au.dk <js@fe1.prime.net>
2023-11-10 13:08:45 +0100

    release

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,13 @@
 from csdp_datastore import ABC
 
-raw_data_path = "C:/Users/au588953"
-output_data_path = "C:/Users/au588953"
-download_token = "20590-LAtzyK3659d5NN-2SMbS"
+raw_data_path = ""
+output_data_path = ""
+download_token = ""
 
 a = ABC(dataset_path = raw_data_path, 
         output_path = output_data_path, 
         output_sample_rate = 128,
         download_token = download_token)
- 
-a.port_data(download_first=True)
+
+a.download() 
+a.port_data()
